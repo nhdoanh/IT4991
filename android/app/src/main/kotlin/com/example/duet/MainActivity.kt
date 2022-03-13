9e58c2ba2aa3c0edf3230013aad822345257c342
HuuDoanh <nhdoanh3012@gmail.com>
2022-03-13 17:15:23 +0700

    [doanhnh] init commit

---
+++ android/app/src/main/kotlin/com/example/duet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.duet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:camera/camera.dart';
+import 'package:duet/duet_app.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  // Fetch the available cameras before initializing the app.
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    logError(e.code, e.description);
+  }
+  runApp(CameraApp());
+}
+
+class CameraApp extends StatelessWidget {
+  const CameraApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CameraExampleHome(),
+    );
+  }
+}
